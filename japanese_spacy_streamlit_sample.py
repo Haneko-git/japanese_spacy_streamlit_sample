f222e8be677bdb9038d8dd024900e667385a9da4
Haneko-git <bayabaya0119@yahoo.co.jp>
2021-05-06 22:44:37 +0900

    sentence change

--- japanese_spacy_streamlit_sample.py
+++ japanese_spacy_streamlit_sample.py
@@ -17,5 +17,5 @@ spacy model download link:https://spacy.io/models/ja
 import spacy_streamlit
 
 models = ["ja_core_news_md", "ja_core_news_sm"]
-default_text = "もうすぐ梅雨。日本のオリンピックはどうなる？"
+default_text = "もうすぐ梅雨。日本のオリンピックはどうなる？？"
 spacy_streamlit.visualize(models, default_text)
